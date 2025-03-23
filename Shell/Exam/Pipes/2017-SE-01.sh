b9fb98179a765d567e99cdfece22b61f5475d131
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-03-23 17:48:00 +0200

    Update 2017-SE-01.sh

--- Shell/Exam/Pipes/2017-SE-01.sh
+++ Shell/Exam/Pipes/2017-SE-01.sh
@@ -1,5 +1,9 @@
 #Намерете имената на топ 5 файловете в текущата директория с най-много hardlinks.
 
+find . -printf '%p %n\n' | sort -n -r -k 2 | head -n 5 | cut -d ' ' -f 1
+#or
+find . -maxdepth 1 -type f -printf '%n %p\n' | sort -k 1 -rn | head -n 5 | cut -d ' ' -f 2
+#or
 find ~ -type f | xargs -I{} stat -c "%h %n" {} | sort -k 1nr -t ' ' | head -n 5 | cut -d ' ' -f2
 #or
 find . -type f | xargs -I{} stat -c "%h %n" {} | sort -k1nr | head -n 5 | awk '{print $2}'
