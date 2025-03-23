62dcda42849bea6cc4998654ea4ee7a13e04d318
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-03-23 14:11:46 +0200

    Update 2016-SE-01.sh

--- Shell/Exam/Pipes/2016-SE-01.sh
+++ Shell/Exam/Pipes/2016-SE-01.sh
@@ -8,5 +8,28 @@
 #Примерен изход:
 #Броят на търсените редовете е 2
 
-
+#Серия от команди:
 cat philip-j-fry.txt | grep -e '^[^a-w]*[2468][^a-w]*$' | wc -l
+#или
+cat philip-j-fry.txt | grep '[02468]' | grep -v '[a-w]' | wc -l
+#или
+cat philip-j-fry.txt | grep -E '[02468]' | grep -E -v '[a-w]' | wc -l
+
+#Shell Script:
+#!/bin/bash
+
+if [ $# -ne 1 ];then
+        echo "Invalid number of arguments"
+        exit 1
+fi
+
+if ! [ -d  "${1}" ];then
+        echo "Not a directory"
+        exit 2
+fi
+
+while read -r file;do
+        if ! [ -e "${file}" ];then
+                echo "${file}"
+        fi
+done < <(find "${1}" -type l 2>/dev/null)
