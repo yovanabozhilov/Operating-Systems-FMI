9ea85392e00436ad857b70396e520ebca5b2cbcb
Yovana Bozhilov <96024394+yovanabozhilov@users.noreply.github.com>
2025-03-23 15:07:20 +0200

    Update 2017-IN-01.sh

--- Shell/Exam/Pipes/2017-IN-01.sh
+++ Shell/Exam/Pipes/2017-IN-01.sh
@@ -3,41 +3,6 @@
 #Забележка: Във файловата система със сигурност съществуват директории, до които нямате
 #достъп.
 
-# Серия от команди:
+# Решение:
 find -user $(id -u) 2>/dev/null | wc -l
 
-# Shell Script:
-#!/bin/bash
-
-if [[ "${#}" -ne 2 ]];then
-        echo "Invalid number of arguments"
-        exit 1
-fi
-
-if [[ ! "${1}" = ^[0-9]+$ ]];then
-        echo "Not a number"
-        exit 2
-fi
-
-if [[ ! "$(whoami)" == "root" ]];then
-        echo "User is not root"
-        exit 3
-fi
-
-users="$(mktemp)"
-number="${1}"
-
-
-ps -e -o user= | sort | uniq > "${users}"
-
-while read user;do
-        sum="$(ps -u "${user}" -o rss= |xargs | tr ' ' '+' | bc)"
-        if [[ "${sum}" -gt "${number}" ]];then
-                highest_rss_process="$(ps -u "${user}" -o rss=,pid= | sort -k 1 -t ' ' -rn | head -n 1 | awk '{print $2}')"
-                #kill -TERM "${highest_rss_process}"
-                sleep 2
-                #kill -KILL "${highes_rss_process}"
-        fi
-done < <(cat "${users}")
-
-rm "${users}"
